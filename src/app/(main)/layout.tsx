946eec07e3198e00015e36698f5d04ea894ed2d6
Panumas Srisook <Panumas.srisook@gmail.com>
2026-02-11 14:08:11 +0700

    fix time out

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "../globals.css";
 import MainLayoutClient from "./MainLayoutClient";
-import IdleTimeoutHandler from "@/components/IdleTimeoutHandler";
 
 export const metadata: Metadata = {
   title: {
@@ -22,7 +21,6 @@ export default async function MainLayout({
   return (
     <MainLayoutClient>
       {children}
-      <IdleTimeoutHandler />
     </MainLayoutClient>
   );
 }
