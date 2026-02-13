a4396e01eb96cde6b3af2127e93b03205fbb1705
Panumas Srisook <Panumas.srisook@gmail.com>
2026-02-13 10:04:52 +0700

    update time to clear logout

--- src/components/IdleTimeoutHandler.tsx
+++ src/components/IdleTimeoutHandler.tsx
@@ -3,6 +3,7 @@
 import { useIdleTimeout } from "@/lib/useIdleTimeout";
 
 export default function IdleTimeoutHandler() {
-  useIdleTimeout(3600);
+  const TIMEOUT = 60 * 60 * 1000; //มิลลิวินาที
+  useIdleTimeout(TIMEOUT);
   return null;
 }
